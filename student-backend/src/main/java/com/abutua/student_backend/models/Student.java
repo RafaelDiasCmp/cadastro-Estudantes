a0e42a32d1dc9a8986e2ba19c4bc7daca5349cfc
Rafael Dias Campos <73750287+RafaelDiasCmp@users.noreply.github.com>
2024-08-26 18:36:20 -0400

    Métodos GET student atualizados

--- student-backend/src/main/java/com/abutua/student_backend/models/Student.java
+++ student-backend/src/main/java/com/abutua/student_backend/models/Student.java
@@ -8,8 +8,71 @@ public class Student {
     private String email; 
     private String phone;
     private int idCurso;
-    private int period;
+    private int periodo;
 
 
+    public Student(int id, String name, String email, String phone, int idCurso, int periodo){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.idCurso = idCurso;
+        this.periodo = periodo;
 
+    }
+
+    public Student(){
+
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public int getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(int periodo) {
+        this.periodo = periodo;
+    }
+
+    
 }
