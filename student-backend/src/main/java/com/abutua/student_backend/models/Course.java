747aaf43af2c486b0c9239b1f9db611ebccc4213
Rafael Dias Campos <73750287+RafaelDiasCmp@users.noreply.github.com>
2024-08-26 18:21:12 -0400

    Classes utilizadas no projeto

---
+++ student-backend/src/main/java/com/abutua/student_backend/models/Course.java
@@ -0,0 +1,9 @@
+package com.abutua.student_backend.models;
+
+public class Course {
+ 
+    //Atributos
+    private int id;
+    private String name;
+
+}
---
+++ student-backend/src/main/java/com/abutua/student_backend/models/Student.java
@@ -0,0 +1,15 @@
+package com.abutua.student_backend.models;
+
+public class Student {
+    
+    //Atributos
+    private int id;
+    private String name;
+    private String email; 
+    private String phone;
+    private int idCurso;
+    private int period;
+
+
+
+}
